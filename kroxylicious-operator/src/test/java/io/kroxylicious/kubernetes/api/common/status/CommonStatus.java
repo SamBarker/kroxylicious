53d05e2554790307e4f6615b380e68a952ed6d66
Sam Barker <sbarker@redhat.com>
2025-03-21 16:21:19 +1300

    Further WIP
    Signed-off-by: Sam Barker <sbarker@redhat.com>
    
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/api/common/status/CommonStatus.java
+++ kroxylicious-operator/src/test/java/io/kroxylicious/kubernetes/api/common/status/CommonStatus.java
@@ -6,9 +6,23 @@
 
 package io.kroxylicious.kubernetes.api.common.status;
 
-import io.kroxylicious.kubernetes.api.v1alpha1.KafkaServiceStatusBuilder;
-
-public class CommonStatus implements io.fabric8.kubernetes.api.builder.Editable<KafkaServiceStatusBuilder>, io.fabric8.kubernetes.api.model.KubernetesResource {
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+@com.fasterxml.jackson.annotation.JsonPropertyOrder({"conditions","observedGeneration"})
+@com.fasterxml.jackson.databind.annotation.JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@javax.annotation.processing.Generated("io.fabric8.java.generator.CRGeneratorRunner")
+@lombok.ToString()
+@lombok.EqualsAndHashCode()
+@io.sundr.builder.annotations.Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = false, builderPackage = "io.fabric8.kubernetes.api.builder", refs = {
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectMeta.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ObjectReference.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.LabelSelector.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Container.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.EnvVar.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.ContainerPort.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.Volume.class),
+        @io.sundr.builder.annotations.BuildableReference(io.fabric8.kubernetes.api.model.VolumeMount.class)
+})
+public class CommonStatus implements io.fabric8.kubernetes.api.builder.Editable<CommonStatusBuilder>, io.fabric8.kubernetes.api.model.KubernetesResource {
     @com.fasterxml.jackson.annotation.JsonProperty("conditions")
     @com.fasterxml.jackson.annotation.JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
     private java.util.List<io.kroxylicious.kubernetes.api.common.Condition> conditions;
@@ -21,8 +35,8 @@ public class CommonStatus implements io.fabric8.kubernetes.api.builder.Editable<
     private Long observedGeneration;
 
     @Override
-    public KafkaServiceStatusBuilder edit() {
-        return new KafkaServiceStatusBuilder(this);
+    public CommonStatusBuilder edit() {
+        return new CommonStatusBuilder(this);
     }
 
     public java.util.List<io.kroxylicious.kubernetes.api.common.Condition> getConditions() {
