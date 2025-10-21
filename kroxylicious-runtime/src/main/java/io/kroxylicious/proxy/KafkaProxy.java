f7043c16a52cef331c5dbdee1a9f4c885555563f
Sam Barker <sam@quadrocket.co.uk>
2025-10-22 11:26:30 +1300

    Make the core count used by the proxy an optional configuration property
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -125,7 +125,7 @@ public final class KafkaProxy implements AutoCloseable {
                     .map(c -> new HostPort(c.getEffectiveBindAddress(), c.getEffectivePort()));
             portConflictDefector.validate(virtualClusterModels, managementHostPort);
 
-            var availableCores = Runtime.getRuntime().availableProcessors();
+            int availableCores = config.eventLoopThreadCount().orElse(Runtime.getRuntime().availableProcessors());
 
             this.managementEventGroup = buildNettyEventGroups("management", availableCores, config.isUseIoUring());
             this.serverEventGroup = buildNettyEventGroups("server", availableCores, config.isUseIoUring());
