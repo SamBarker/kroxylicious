4d7beee75d560b354479df36954676edf76d14e4
Sam Barker <sam@quadrocket.co.uk>
2025-10-22 20:52:30 +1300

    Push defaulting to core count into the config model
    Signed-off-by: Sam Barker <sam@quadrocket.co.uk>

--- kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
+++ kroxylicious-runtime/src/main/java/io/kroxylicious/proxy/KafkaProxy.java
@@ -125,7 +125,7 @@ public final class KafkaProxy implements AutoCloseable {
                     .map(c -> new HostPort(c.getEffectiveBindAddress(), c.getEffectivePort()));
             portConflictDefector.validate(virtualClusterModels, managementHostPort);
 
-            int availableCores = config.eventLoopThreadCount().orElse(Runtime.getRuntime().availableProcessors());
+            int availableCores = config.activeEventLoopThreadCount();
 
             this.managementEventGroup = buildNettyEventGroups("management", availableCores, config.isUseIoUring());
             this.serverEventGroup = buildNettyEventGroups("server", availableCores, config.isUseIoUring());
